c6c59bc6b2277aea00dcdf4e28827604ef5d5843
Evgeniy <chilly_bang@yahoo.de>
2014-03-10 16:20:36 +0100

    Update jquery.microdata.js

--- jquery.microdata.js
+++ jquery.microdata.js
@@ -32,10 +32,10 @@
             
             float:     function(value, el) { return (/^\d+([.,]\d*)?$/).test(value); },
             
-			// ADD 6.03.2014: Number (both of int and float) Validation
-			 number:  function(value, el) { return (/(\d+(.\d+)?)/).test(value); },
+	// ADD 6.03.2014: Number (both of int and float) Validation
+	    number:  function(value, el) { return (/(\d+(.\d+)?)/).test(value); },
 			 
-			 // ADD 6.03.2014: Date Validation
+	// ADD 6.03.2014: Date Validation
             date:  function(value, el) { return (/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/).test(value); },
 			
             // TODO: need a proper datetime validator
